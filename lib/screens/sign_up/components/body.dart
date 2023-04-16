362dcea2fc4301f8021046d842e7c4f527f052ff
Dzhanaliev Aktan <88595595+MorgDzh@users.noreply.github.com>
2023-04-16 20:50:11 +0600

    Finished sign_up screen :comet:

--- lib/screens/sign_up/components/body.dart
+++ lib/screens/sign_up/components/body.dart
@@ -1,4 +1,9 @@
+import 'package:e_commerce/components/custom_suffix_icon.dart';
+import 'package:e_commerce/components/default_button.dart';
+import 'package:e_commerce/components/form_error.dart';
+import 'package:e_commerce/components/social_card.dart';
 import 'package:e_commerce/constants.dart';
+import 'package:e_commerce/screens/sign_up/components/sign_up_form.dart';
 import 'package:e_commerce/size_config.dart';
 import 'package:flutter/material.dart';
 
@@ -9,38 +14,49 @@ class Body extends StatelessWidget {
   Widget build(BuildContext context) {
     return SizedBox(
       width: double.infinity,
-      child: Column(
-        children: [
-          Text("Register Account", style: headingStyle),
-          Text(
-            "Complete your details or continue \nwith social media",
-            textAlign: TextAlign.center,
+      child: Padding(
+        padding:
+            EdgeInsets.symmetric(horizontal: getProportionateScreenWidth(20)),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              SizedBox(height: SizeConfig.screenHeight * 0.02), // 2 %
+              Text("Register Account", style: headingStyle),
+              Text(
+                "Complete your details or continue \nwith social media",
+                textAlign: TextAlign.center,
+              ),
+              SizedBox(
+                  height: SizeConfig.screenHeight * 0.07), // 8% of total height
+              SignUpForm(),
+              SizedBox(height: SizeConfig.screenHeight * 0.07),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  SocialCard(
+                    icon: "assets/icons/google-icon.svg",
+                    press: () {},
+                  ),
+                  SocialCard(
+                    icon: "assets/icons/facebook-2.svg",
+                    press: () {},
+                  ),
+                  SocialCard(
+                    icon: "assets/icons/twitter.svg",
+                    press: () {},
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: getProportionateScreenHeight(20),
+              ),
+              Text(
+                "By continuing your confirm that you agree \nwith our Term and Condition",
+                textAlign: TextAlign.center,
+              )
+            ],
           ),
-          SignUpForm(),
-        ],
-      ),
-    );
-  }
-}
-
-class SignUpForm extends StatefulWidget {
-  final _formKey = GlobalKey<FormState>();
-  String? email;
-  String? password;
-  String? conform_password;
-
-  @override
-  State<SignUpForm> createState() => _SignUpFormState();
-}
-
-class _SignUpFormState extends State<SignUpForm> {
-  @override
-  Widget build(BuildContext context) {
-    return Form(
-      child: Column(
-        children: [
-          
-        ],
+        ),
       ),
     );
   }
