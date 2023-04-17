9ec0faa1d576c5bfc9477fa9b4949d8d85d0badf
Dzhanaliev Aktan <88595595+MorgDzh@users.noreply.github.com>
2023-04-17 21:23:09 +0600

    Updated home screen (added home header) :tada:

--- lib/screens/home/components/body.dart
+++ lib/screens/home/components/body.dart
@@ -1,4 +1,4 @@
-import 'package:e_commerce/constants.dart';
+import 'package:e_commerce/screens/home/components/home_header.dart';
 import 'package:e_commerce/size_config.dart';
 import 'package:flutter/material.dart';
 
@@ -11,43 +11,8 @@ class Body extends StatelessWidget {
       child: SingleChildScrollView(
         child: Column(
           children: [
-            Padding(
-              padding: EdgeInsets.symmetric(
-                  horizontal: getProportionateScreenWidth(20)),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Container(
-                    width: SizeConfig.screenWidth * 0.6, // 60% of width
-                    // height: 50,
-                    decoration: BoxDecoration(
-                        color: kSecondaryColor.withOpacity(0.1),
-                        borderRadius: BorderRadius.circular(15)),
-                    child: TextField(
-                      onChanged: (value) {
-                        // search value
-                      },
-                      decoration: InputDecoration(
-                        enabledBorder: InputBorder.none,
-                        focusedBorder: InputBorder.none,
-                        hintText: "Search Product",
-                        prefixIcon: Icon(Icons.search),
-                        contentPadding: EdgeInsets.symmetric(
-                            horizontal: getProportionateScreenWidth(20),
-                            vertical: getProportionateScreenWidth(9)),
-                      ),
-                    ),
-                  ),
-                  Container(
-                    height: getProportionateScreenWidth(46),
-                    width: getProportionateScreenWidth(46),
-                    decoration: BoxDecoration(
-                        color: kSecondaryColor.withOpacity(0.1),
-                        shape: BoxShape.circle),
-                  )
-                ],
-              ),
-            )
+            SizedBox(height: getProportionateScreenWidth(20)),
+            HomeHeader(),
           ],
         ),
       ),
