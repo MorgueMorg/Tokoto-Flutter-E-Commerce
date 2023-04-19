c51cf300da8bc0645313d532d903c8c2676670a5
Dzhanaliev Aktan <88595595+MorgDzh@users.noreply.github.com>
2023-04-19 08:44:27 +0600

    Added custom AppBar for details screen :yo_yo:

--- lib/screens/details/details_screen.dart
+++ lib/screens/details/details_screen.dart
@@ -1,16 +1,26 @@
 import 'package:e_commerce/models/Product.dart';
+import 'package:e_commerce/screens/details/components/custom_app_bar.dart';
 import 'package:flutter/material.dart';
 
 class DetailsScreen extends StatelessWidget {
   static String routeName = "/details";
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    final ProductDetailsArguments agrs =
+        ModalRoute.of(context)!.settings.arguments as ProductDetailsArguments;
+    return Scaffold(
+      // By default background color is white
+      backgroundColor: const Color(0xFFF5F6F9),
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(AppBar().preferredSize.height),
+        child: CustomAppBar(rating: agrs.product.rating),
+      ),
+    );
   }
 }
 
 // But also need to pass product to details screen
-// And use name route so we need to create a arguments class
+// And use name route so I need to create a arguments class
 
 class ProductDetailsArguments {
   final Product product;
